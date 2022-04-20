d9018d471aa964e3a81e6646137e103cab2859dc
Anand-T-S <100193636+Anand-T-S@users.noreply.github.com>
2022-04-20 22:59:43 +0530

    ORM queries...

--- todoapp/models.py
+++ todoapp/models.py
@@ -11,6 +11,23 @@ class Todos(models.Model):
   def __str__(self):
     return self.task_name
 
+
+#ORM queries
+#object -relational mapper
+#ORM query for creating resource
+#ref_name=Modelname(fieldname="value")
+#ref_name.save()
+
+#or
+#modelname.objects.create(fieldname="value")
+
 #orm query for fetching all records
 #ref name=model name.objects.all()
 #qs=Todos.objects.all()
+
+#orm queries for filtering,excluding,to get a specific value
+#ref_name=modelname.objects.filter(fieldname="value")
+
+#ref_name=modelname.objects.all().exclude(fieldname="value")
+
+#ref_name=modelname.objects.get(fieldname="value")
