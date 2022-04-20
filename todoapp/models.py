2cec918cc86ad2c09146d44f50e67129add8afff
admin <anandtsreenivasan2000@gmail.com>
2022-04-20 13:33:05 +0530

    model updated

--- todoapp/models.py
+++ todoapp/models.py
@@ -7,3 +7,10 @@ class Todos(models.Model):
   user=models.CharField(max_length=80)
   date=models.DateField(auto_now_add=True)
   status=models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.task_name
+
+#orm query for fetching all records
+#ref name=model name.objects.all()
+#qs=Todos.objects.all()
